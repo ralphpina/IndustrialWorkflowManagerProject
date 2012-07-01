a7607ade63950966b02285e4e9fe47de14418aab
Ralph Pina <ralph.pina@gmail.com>
2012-07-01 15:41:42 -0500

    Industrial Workflow Manager by teamjacob

---
+++ IndustrialWorkflowManagerProject/apps/IndustrialWorkflowManager/common/js/IndustrialWorkflowManager.js
@@ -0,0 +1,7 @@
+
+// Worklight comes with the jQuery framework bundled inside. If you do not want to use it, please comment out the line below.
+window.$ = WLJQ;
+
+function wlCommonInit(){
+	// Common initialization code goes here
+}
---
+++ IndustrialWorkflowManagerProject/apps/IndustrialWorkflowManager/common/js/auth.js
@@ -0,0 +1,39 @@
+/*
+* Licensed Materials - Property of IBM
+* 5725-G92 (C) Copyright IBM Corp. 2006, 2012. All Rights Reserved.
+* US Government Users Restricted Rights - Use, duplication or
+* disclosure restricted by GSA ADP Schedule Contract with IBM Corp.
+*/
+
+/* Authenticator skeleton; Uncomment and implement if your application requires authentication. */
+/*
+var Authenticator = function () {
+
+    // Private members should be placed here
+
+    return {
+        // Public members should be placed here
+        init : function () {
+              // Authenticator initialization code should be placed here
+        },
+
+        isLoginFormResponse : function (response) {
+              // Returns true/false
+              
+              return false;
+        },
+        
+        onBeforeLogin : function (response, username, onSubmit, onCancel) {
+              
+        },
+
+       onShowLogin: function() {
+              
+       },
+        
+       onHideLogin: function(){
+              
+       }               
+    }; 
+}();
+*/
